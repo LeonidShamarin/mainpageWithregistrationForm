9befbc4c7774eac4c9a5c13865b5e8e5582a8244
Leonid <leonideko1@gmail.com>
2024-02-10 13:07:11 +0200

    fixed style fotm-modal

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const closeButton = document.getElementById("close-icon");
-//   const openModalButtons = document.querySelectorAll(".open-modal-btn");
-//   const contactForm = document.getElementById("contact-form");
-
-//   closeButton.addEventListener("click", function () {
-//       contactForm.style.display = "none";
-//   });
-
-//   openModalButtons.forEach(function (button) {
-//       button.addEventListener("click", function () {
-//           contactForm.style.display = "block";
-//       });
-//   });
-// });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   var openModalButton = document.getElementById('open-modal-my');
-//   var contactForm = document.getElementById('contact-form');
-
-//   // Додаємо обробник події на кнопку "open-modal"
-//   openModalButton.addEventListener('click', function() {
-//       contactForm.style.display = 'block'; // Показуємо форму
-//   });
-// });
-
-//!
-
 let modal = document.getElementById("modal");
 let closeButton = document.getElementById("close-icon");
 function openModal() {
@@ -41,28 +13,4 @@ window.onclick = function (event) {
   }
 };
 // setTimeout(openModal, 2000);
-//!
-// document.addEventListener("DOMContentLoaded", function () {
-//   const closeButton = document.querySelector(".close-icon");
-//   const openModalButtons = document.querySelectorAll(".button");
-
-//   closeButton.addEventListener("click", closeModal);
-//   openModalButtons.forEach(button => {
-//     button.addEventListener("click", openModal);
-//   });
-
-//   function openModal() {
-//     const modal = document.getElementById("modal");
-//     const modalContent = document.querySelector('.contact-form-details');
-//     modal.style.display = "block";
-//     modalContent.style.display = "block";
-//   }
-
-//   function closeModal() {
-//     const modal = document.getElementById("modal");
-//     const modalContent = document.querySelector('.contact-form-details');
-//     modal.style.display = "none";
-//     modalContent.style.display = "none";
-//   }
-// });
 
